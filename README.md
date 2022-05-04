43557a81b186763fbcb2e8309ebd5dfa96eed1ce
Bastien Nocera <hadess@hadess.net>
2022-05-04 21:45:36 +0200

    greaseweazle-floppy-drive-test

---
+++ README.md
@@ -0,0 +1,11 @@
+# Greaseweazle Floppy Drive Test
+
+I use this to test PC floppy drives in read and write operations in
+an automated way.
+
+1. Make sure that a writable floppy is in the drive and the `gw` command is accessible
+2. Run `./test.sh`
+
+## License
+
+[GPL v3 or later](https://spdx.org/licenses/GPL-3.0-or-later.html).
---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# See https://github.com/keirf/greaseweazle/issues/193
+gw info  2>&1 | grep -q "Not found"
+if [ $? == 0 ] ; then
+	echo "Error: did not detect any Greaseweazle"
+	exit 1
+fi
+
+echo "Insert a non-write protected floppy. Floppy will be erased."
+echo "Press Enter to continue, Ctrl+C to abort"
+read
+
+FULLIMG=`mktemp --suffix=.img`
+
+gw write --format ibm.1440 144mb-empty.img
+echo write empty retval $?
+gw read --format ibm.1440 $FULLIMG
+diff $FULLIMG 144mb-empty.img
+if [ $? != 0 ] ; then
+	echo "Error: Failed to read/write empty disk image"
+	exit 1
+fi
+echo "Read/Write test #1 OK"
+rm -f $FULLIMG
+
+gw write --format ibm.1440 144mb-full.img
+gw read --format ibm.1440 $FULLIMG
+diff $FULLIMG 144mb-full.img
+if [ $? != 0 ] ; then
+	echo "Error: Failed to read/write full disk image"
+	exit 1
+fi
+echo "Read/Write test #2 OK"
